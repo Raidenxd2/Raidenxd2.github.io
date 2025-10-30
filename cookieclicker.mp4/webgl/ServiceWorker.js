25eb0d21337396b237880e7279b66073a34d01ef
Raidenxd2 <64755433+Raidenxd2@users.noreply.github.com>
2025-10-30 13:38:30 -0400

    add cc1 webgl

---
+++ cookieclicker.mp4/webgl/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "raiden-Cookieclicker.mp4-11_17 (main)";
+const contentToCache = [
+    "Build/CC1_WebGL-PWA.loader.js",
+    "Build/CC1_WebGL-PWA.framework.js",
+    "Build/CC1_WebGL-PWA.data",
+    "Build/CC1_WebGL-PWA.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
